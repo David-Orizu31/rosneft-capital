55c1a13cb1868301065c9053075e2a890f917d97
David-Orizu31 <117983147+Digital-Roah@users.noreply.github.com>
2025-09-30 15:56:14 +0100

    Completed 5 pages of the website

--- js/scripts.js
+++ js/scripts.js
@@ -26,3 +26,33 @@ document.querySelectorAll("#languageList .dropdown-item").forEach(function(el) {
     });
 });
 });
+
+ //Get the button
+ var mybutton = document.getElementById("myBtn");
+
+ // When the user scrolls down 20px from the top of the document, show the button
+ window.onscroll = function() {scrollFunction()};
+ 
+ function scrollFunction() {
+   if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+     mybutton.style.display = "block";
+   } else {
+     mybutton.style.display = "none";
+   }
+ }
+ 
+ // When the user clicks on the button, scroll to the top of the document
+ function topFunction() {
+   document.body.scrollTop = 0;
+   document.documentElement.scrollTop = 0;
+ }
+
+$(function () {
+    $(document).scroll(function () {
+        var headerheight = $('.site-header').height();
+        var navheight = $('.navbar').height();
+        var totalheight = headerheight + navheight;
+      var $nav = $(".navbar");
+      $nav.toggleClass('sticky', $(this).scrollTop() > totalheight);
+    });
+});
